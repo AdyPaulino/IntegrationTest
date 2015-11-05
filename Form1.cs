7d1aa0e6d719dd51afa5240d2dec29a704a61c2d
zmk161 <mzhang3967@2A207-B08.conestogac.on.ca>
2015-11-05 11:55:54 -0500

    update the method and add the combbox
    update the method and add the combbox

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,8 @@ namespace IntegrationTest
 {
     public partial class Form1 : Form
     {
+
+        private int country;
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +22,61 @@ namespace IntegrationTest
         private void label1_Click(object sender, EventArgs e)
         {
 
+
+
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox cmb = (ComboBox)sender;
+            country = cmb.SelectedIndex;
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ageButton_Click(object sender, EventArgs e)
+        {
+            switch (country)
+            {
+                case 0 :
+                    //CheckAge18();
+                    break;
+                case 2:
+                    //CheckAge21();
+                    break;
+                case 3:
+                    //CheckAge18();
+                    break;
+                case 4:
+                    //CheckAge0();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        
     }
 }
