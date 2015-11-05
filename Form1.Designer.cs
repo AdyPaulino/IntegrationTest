6ab9f8a8d110ec298159569768bb5469d27dc567
AdyPaulino <apaulino7908@2A207-B09.conestogac.on.ca>
2015-11-05 11:24:50 -0500

    Age cheking
    form fields added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.age = new System.Windows.Forms.TextBox();
+            this.ageButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Age:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // age
+            // 
+            this.age.Location = new System.Drawing.Point(47, 20);
+            this.age.Name = "age";
+            this.age.Size = new System.Drawing.Size(100, 20);
+            this.age.TabIndex = 1;
+            // 
+            // ageButton
+            // 
+            this.ageButton.Location = new System.Drawing.Point(174, 18);
+            this.ageButton.Name = "ageButton";
+            this.ageButton.Size = new System.Drawing.Size(98, 34);
+            this.ageButton.TabIndex = 2;
+            this.ageButton.Text = "Am I allowed to drink?";
+            this.ageButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.ageButton);
+            this.Controls.Add(this.age);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Alcohol";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox age;
+        private System.Windows.Forms.Button ageButton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace IntegrationTest
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
